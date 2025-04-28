86e5f1844dbaf45ae3ce815f6afa5acb0759aaa4
saiynam <Saiynam884@gmail.com>
2025-04-29 00:25:11 +0700

    add command auto Role EP:1
    Make discord auto Role and XP system first step

--- coding/main.py
+++ coding/main.py
@@ -1,14 +1,43 @@
 import discord
 from discord.ext import commands
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents = intents)
+
+XP_FILE = "xp_data.json"
+
+if os.path.exists(XP_FILE):
+    with open(XP_FILE, "r") as f:
+        xp_data = json.load(f)
+    
+else:
+    xp_data = {}
+
+#   ຕັ້ງບົດບາດບ່ອນີ້
+
+LEVEL_ROLE = {
+    1 : 1362824931124969472 , # Role ID ສຳລັບເວລ 1
+    15 : 1362826484154106209 , # Role ID ສຳລັບເວລ 15
+    20 : 1362826576345174036 , # Role ID ສຳລັບເວລ 20
+    40 : 1362826944411861002 , # Role ID ສຳລັບເວລ 40
+    50 : 1362827025361928282 , # Role ID ສຳລັບເວລ 50
+}
+
+def save_xp():
+    with open(XP_FILE, "w") as f :
+        json.dump(xp_data, f)
+
+def get_level(xp):
+    return xp // 100        #   100 xp = 1 level
+
 
 @bot.event
 async def on_ready():
@@ -42,5 +71,8 @@ async def on_message(message):
 
     await bot.process_commands(message)
     print(messageInput)
+
+
+
 
 bot.run(TOKEN)
